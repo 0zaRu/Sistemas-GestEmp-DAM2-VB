1ad805a3bb04c738bfd4f21632a979c8d96bd8d3
OzaRu <aruberuto03@gmail.com>
2023-11-14 20:58:28 +0100

    commit

--- Tema 4/TRES_EN_RAYA.py
+++ Tema 4/TRES_EN_RAYA.py
@@ -10,9 +10,50 @@ def impresion(tablero):
         print()
         print("  -------------")
         i+=1
+    
+    print("\n(1H, 2V --> EJ: 23)")
 
+def nuevoMovimiento(nMovimiento, tablero):
+    h, v = input("Introduce la jugada: ")
+    x, y = int(h), int(v)
+    
+    if x < 1 or y < 1 or x > 3 or y > 3:
+        return False, tablero 
+    
+    if nMovimiento%2==0:
+        tablero[x-1][y-1] = "X"
+    else:
+        tablero[x-1][y-1] = "O"
+    
+    return True, tablero
+
+
+def comprueba3Enraya(tablero):
+    return True        
+    
 
 tablero = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-
 impresion(tablero)
 
+victoria = False
+i = 0
+
+while not victoria:
+    i+=1
+    valido, tablero = nuevoMovimiento(i, tablero)
+    
+    if valido is True:
+        impresion(tablero)
+    else:
+        print("El rango de valores introducido no es correcto...")
+        i -= 1
+    
+    if i >= 5:
+        victoria = comprueba3Enraya(tablero)
+        if victoria:
+            if i%2 == 0:
+                print("\nEnhorabuena!, ha ganado el jugador X\n"\
+                        "====================================\n")
+            else:
+                print("\nEnhorabuena!, ha ganado el jugador O\n"\
+                        "====================================\n")    
