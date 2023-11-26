7fdbc6e3bebb7053f51c40eaf0b3b44855ab9efa
OzaRu <114357633+0zaRu@users.noreply.github.com>
2023-11-26 20:03:59 +0100

    commit

--- Tema 5/Ejercicios/Decoradores.py
+++ Tema 5/Ejercicios/Decoradores.py
@@ -1,12 +1,28 @@
 from os import system
 
+def almacenaDic(fichero_log):
+    def decFuncRecorrido(funcion):
+        def decorador(diccionario):
+            with open(fichero_log, 'a') as opened_file:
+                output = funcion(diccionario)
+                opened_file.write(f"{output}\n")
+                return "Fichero escrito correctamente"
+        return decorador
+    return decFuncRecorrido
+
+
+@almacenaDic('ficherosalida.log')
 def recorreDiccionario(asignaturas):
+    resultado = ""
+
     for a in asignaturas:
-        print(a)
+        resultado += a + "\n"
         for b in asignaturas[a]:
-            print("\t", b)
+            resultado += "\t" + b + "\n"
             for n, v in asignaturas[a][b].items():
-                print("\t\t",n,":",v)
+                resultado += "\t\t" + n + ": " + str(v) + "\n"
+
+    return resultado
 
 def recargaDiccionario():
     listaNombre = ["Juan", "Maria", "Elena"]
--- hundir-la-flota.py
+++ hundir-la-flota.py
@@ -1,18 +1,9 @@
 # chr(27)+"[9;31m"+"X"+chr(27)+"[0;37m"
-#recuerda esto cuando se toca un barco
-
 import os
 from random import *
 
-nBarcos = {
-    "C": [4, 1],
-    "T": [3, 2],
-    "D": [2, 3],
-    "U": [1, 4]
-}
-
 class Tablero():
-    t = []
+    t: list
 
     def __init__(self, nTablero = 2, nLineas = 10, nColumnas = 10, valorCasilla = " "):
         tableros = []
@@ -26,7 +17,20 @@ class Tablero():
             tableros.append(tablero)
         self.t = tableros
 
-def impresion(tableros, turno):
+    class RegistroConstruccionT():
+        nBarcos = {
+            "C": [4, 1],
+            "T": [3, 2],
+            "D": [2, 3],
+            "U": [1, 4]}
+
+        horizontal: bool
+        pH: int
+        pV: int
+        letraBarco: str
+        tamBarco: int
+
+def impresion(tableros):
     os.system("cls")
 
     cont = 0
@@ -52,135 +56,66 @@ def impresion(tableros, turno):
     print(("\t   ╚"+("═══╩"*9)+"═══╝ \t")*2)
     print((" ")*27+"JUGADOR 1"+(" ")*47+"JUGADOR 2\n\n")
 
-def rodeaCasilla(t, h, v):
-    #tuuve algun idea de hacerlo con dos listas en 2 bucles anidados haciendo los conjuntos
-    #de posiblidad de [-1, 0, 1] para h y v pero no me terminó de salir, cuando lo consiga modificaré la entrega
-
-    if h+1 < len(t) and v   < len(t[0]) and t[h+1][v]   == " ": t[h+1][v]   = "."
-    if h+1 < len(t) and v+1 < len(t[0]) and t[h+1][v+1] == " ": t[h+1][v+1] = "."
-    if h+1 < len(t) and v-1 >= 0        and t[h+1][v-1] == " ": t[h+1][v-1] = "."
-    if h   < len(t) and v+1 < len(t[0]) and t[h]  [v+1] == " ": t[h]  [v+1] = "."
-    if h   < len(t) and v-1 >= 0        and t[h]  [v-1] == " ": t[h]  [v-1] = "."
-    if h-1 >= 0     and v   < len(t[0]) and t[h-1][v]   == " ": t[h-1][v]   = "."
-    if h-1 >= 0     and v+1 < len(t[0]) and t[h-1][v+1] == " ": t[h-1][v+1] = "."
-    if h-1 >= 0     and v-1 >= 0        and t[h-1][v-1] == " ": t[h-1][v-1] = "."
+def rodeaCasilla(t, pH, pV):
+    for h in [-1, 0, 1]:
+        for v in [-1, 0, 1]:
+            if pH+h in range(len(t)) and pV+v in range(len(t[0])) and t[pH+h][pV+v] == " ": 
+                t[pH+h][pV+v] = "."
 
     return t
 
-def gestionaEspacio(t, horizontal, letraBarco, tBarco, pH, pV):
+def gestionaEspacio(t, c: Tablero.RegistroConstruccionT):
     #primero compruebo que en los espacios se puede poner un barco
-    for i in range(tBarco):
-        if horizontal:
-            if t[pH+i][pV] != " ": return False, t
-        if not horizontal:
-            if t[pH][pV+i] != " ": return False, t
+    for i in range(c.tamBarco):
+        if c.horizontal:
+            if t[c.pH+i][c.pV] != " ": return False, t
+        if not c.horizontal:
+            if t[c.pH][c.pV+i] != " ": return False, t
     
-    #luego lo pongo
-    for i in range(tBarco):
-        if horizontal:
-            t[pH+i][pV] = letraBarco
-
-        if not horizontal:
-            t[pH][pV+i] = letraBarco
-
-    #escribo puntos filtrando por la letra de los barcos actuales para ahorrar vueltas
-    for i in range(len(t)):
-        for j in range(len(t[0])):
-            if t[i][j] == letraBarco: t = rodeaCasilla(t, i, j)
+    #luego lo pongo y guardo sus posiciones
+    casillasBarco = []
+    for i in range(c.tamBarco):
+        if c.horizontal:
+            t[c.pH+i][c.pV] = c.letraBarco
+            casillasBarco.append([c.pH+i, c.pV])
+        
+        if not c.horizontal:
+            t[c.pH][c.pV+i] = c.letraBarco
+            casillasBarco.append([c.pH, c.pV+i])
+        
+    #escribo puntos alrededor del barco que acabo de colocar
+    for casilla in casillasBarco:
+        t = rodeaCasilla(t, casilla[0], casilla[1])
 
     return True, t
 
 def generaBarcos(t):
+    c = Tablero.RegistroConstruccionT
+
     #recorro el diccionario de barcos
-    for n, v in nBarcos.items():        
-        pH, pV = -1, -1
+    for n, v in c.nBarcos.items():        
+        c.pH, c.pV = -1, -1
         #doy tantas vueltas como barcos haya que poner del tipo en el que estamos
         for cantBarcos in range(v[1]):
             #establezco la orientación del próximo barco
-            horizontal = True if randint(0, 1) == 0 else False
+            c.horizontal = True if randint(0, 1) == 0 else False
             salir = False
 
             #doy vueltas seleccionando casillas hasta encontrar una válida y colocar el barco
             while not salir:
-                pH = randint(0, len(t)-v[0]) if horizontal else randint(0, len(t)-1)
-                pV = randint(0, len(t[0])-v[0]) if not horizontal else randint(0, len(t[0])-1)
-
-                salir, t = gestionaEspacio(t, horizontal, n, v[0], pH, pV)
+                c.pH = randint(0, len(t)-v[0]) if c.horizontal else randint(0, len(t)-1)
+                c.pV = randint(0, len(t[0])-v[0]) if not c.horizontal else randint(0, len(t[0])-1)
+                
+                c.letraBarco = n; c.tamBarco = v[0]
+                salir, t = gestionaEspacio(t, c)
     
     return t
 
-""" METODOS QUE FALTA IMPLEMENTAR
-def nuevoMovimiento(tablero, jugador):
-    try:
-        y = int(input("\n\tEs el turno de "+jugador+".\n\tIntroduce el nº de columna: "))-1
-        if y < 0 or y > 6:
-            return False, tablero 
-        
-        for i in range(7):
-            if tablero[6-i][y] == ".":
-                tablero[6-i][y] = jugador
-                return True, tablero
-        
-    except:
-        pass
 
-    return False, tablero
 
-def compruebaCasilla(x, y, tablero):
-    rangos = [[".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."]]
-    
-    for i in range(4):
-        if y+i <= 5: rangos[0][i] = tablero[x][y+i]      #Comprueba vertical creciento
-        if x+i <= 6: rangos[1][i] = tablero[x+i][y]      #Comprueba horizontal derecha
-        if y+i <= 5 and x+i <= 6: rangos[2][i] = tablero[x+i][y+i] #Comprobar diagonal creciente derecha
-        if y+i <= 5 and x-i >= 0: rangos[3][i] = tablero[x-i][y+i] #Comprobar diagonal creciente izquierda
-        
-    for i in range(4):
-        if all(valor == "X" for valor in rangos[i]) or all(valor == "O" for valor in rangos[i]): 
-            return True
-    
-    return False
-
-def comprueba4Enraya(tablero):
-    for i in range(7):
-        for j in range(6):
-            if tablero[i][j] != "." and compruebaCasilla(i, j, tablero):
-                return True
-    
-    return False
-"""
-
-t = Tablero(2, 10, 10, " ")
+t = Tablero()
 
 t.t[0] = generaBarcos(t.t[0])
 t.t[1] = generaBarcos(t.t[1])
 
-impresion(t.t, 1)
-
-
-""" JUGABILIDAD QUE FALTA POR IMPLEMENTAR
-i = 1
-while True:
-    if i%2 == 0: jugador = "Jugador 1"
-    else :       jugador = "Jugador 2"
-
-    valido, tablero = nuevoMovimiento(tablero, jugador)    
-
-    if valido is True:
-        impresion(tablero, i+1)
-    else:
-        print("\tRango de valores introducido no es correcto, "\
-            "ya hay colocada una ficha o el carácter no es válido ...\n\n")
-        i -= 1
-
-    if comprueba4Enraya(tablero) and i>=4:
-        print("\n\tEnhorabuena!, ha ganado el jugador "+jugador+"\n"\
-                "\t====================================\n")
-        break
-    
-    i+=1    
-else:
-    print("\n\tEMPATE, NINGUNO DE LOS JUGADORES HA GANADO\n"\
-            "\t==========================================\n")
-            
-"""
+impresion(t.t)
